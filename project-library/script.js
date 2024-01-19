4de792c38567c2b8d58dacdd52bdb7ed6398729d
Jose Crespo <92603352+Jose-C0@users.noreply.github.com>
2024-01-19 07:17:26 +0000

    add devcontainer

---
+++ project-library/script.js
@@ -0,0 +1,27 @@
+const myLibrary = [];
+
+console.log("jola");
+
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+// TODO: 3. Escribe una función que recorra el array y muestre cada libro en la página. 
+// Puedes mostrarlos en algún tipo de tabla, o cada uno en su propia "tarjeta". 
+// Por ahora puede ser útil añadir manualmente algunos libros a tu matriz para que puedas ver la pantalla.
+
+// TODO: 4. Añade un botón "NUEVO LIBRO" que muestre un formulario que permita 
+// a los usuarios introducir los datos del nuevo libro: 
+// autor, título, número de páginas, si se ha leído y cualquier otra cosa que desees. 
+// Tú decides cómo mostrar este formulario.
+
+// TODO: 5. Añade un botón en la pantalla de cada libro para eliminarlo de la biblioteca.
+
+// TODO: 6. Añade un botón en la pantalla de cada libro para cambiar su estado de lectura. 
+// Para facilitar esto usted querrá crear la función que cambia el estado de lectura de un 
+// libro en su instancia de prototipo de Libro.
