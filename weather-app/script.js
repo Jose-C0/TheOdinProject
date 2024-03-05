788894418c367dcb51d754ab5a3c75578a8fa043
Jose Crespo <92603352+Jose-C0@users.noreply.github.com>
2024-03-05 22:55:42 +0000

    update

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,12 +1,40 @@
+const divContainer = document.querySelector(".container");
+
+function setP(property) {
+  const p = document.createElement("p");
+  p.textContent = property;
+  return p;
+}
+
+const getWeather = async () => {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=fc5fe7e276864dcd96b42231240403 &q=London&aqi=no",
+    { mode: "cors" }
+  );
+  const data = await response.json();
+  
+  console.log(data);
+
+  divContainer.appendChild(setP("name: " + data.location.name));
+  divContainer.appendChild(setP("country: " + data.location.country));
+  divContainer.appendChild(setP("lat: " + data.location.lat));
+  divContainer.appendChild(setP("localtime: " + data.location.localtime));
+  divContainer.appendChild(setP("localtime_epoch: " + data.location.localtime_epoch));
+  divContainer.appendChild(setP("lon: " + data.location.lon));
+  divContainer.appendChild(setP("region: " + data.location.region));
+  divContainer.appendChild(setP("tz_id: " + data.location.tz_id));
+};
+
+getWeather();
+
+/*
 const img = document.querySelector("img");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=4VR9IjvvONKqegzFWT7XbQonkiX3500w&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+async function getCats() {
+  const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=4VR9IjvvONKqegzFWT7XbQonkiX3500w&s=cats", { mode: "cors" });
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
+}
+
+getCats();
+*/
