3b6ee09525e5a9a2451629a898624df9a6ad73ff
Jose Crespo <rocj1209@gmail.com>
2024-03-17 16:23:11 -0400

    weather-app list of city

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,22 +1,15 @@
 const divContainer = document.querySelector(".container");
+const citySelect = document.getElementById("city-select");
+const myForm = document.getElementById("myForm");
 
 function setP(property) {
   const p = document.createElement("p");
   p.textContent = property;
   return p;
 }
-
-const getWeather = async () => {
-  const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=fc5fe7e276864dcd96b42231240403 &q=London&aqi=no",
-    { mode: "cors" }
-  );
-  const data = await response.json();
-
-  console.log(data);
-
-  divContainer.appendChild(setP("Name: " + data.location.name) );
- 
+let setResult = (data) => {
+  divContainer.innerHTML = "";
+  divContainer.appendChild(setP("Name: " + data.location.name));
   divContainer.appendChild(setP("Country: " + data.location.country));
   divContainer.appendChild(setP("Lat: " + data.location.lat));
   divContainer.appendChild(setP("Localtime: " + data.location.localtime));
@@ -26,20 +19,50 @@ const getWeather = async () => {
   divContainer.appendChild(setP("Lon: " + data.location.lon));
   divContainer.appendChild(setP("Region: " + data.location.region));
   divContainer.appendChild(setP("Tz_id: " + data.location.tz_id));
-
-  //Todo: img.src = data.data.images.original.url;
 };
 
-getWeather();
+const getWeather = async (city) => {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=fc5fe7e276864dcd96b42231240403 &q=${city}&aqi=no`,
+    { mode: "cors" }
+  );
 
+  const data = await response.json();
+
+  return setResult(data);
+};
+
+let listaPara = [];
 /*
-const img = document.querySelector("img");
+async function ejemplo() {
+  //current.city.list
+  const response = await fetch("./current.city.list.json");
 
-async function getCats() {
-  const response = await fetch("https://api.giphy.com/v1/gifs/translate?api_key=4VR9IjvvONKqegzFWT7XbQonkiX3500w&s=cats", { mode: "cors" });
-  const catData = await response.json();
-  img.src = catData.data.images.original.url;
+  const name = await response.json();
+
+  name.forEach((x) => {
+    listaPara.push(x.name);
+        // Esta funcion es para crear una lista de todos los paises en el formulario
+
+    const option = document.createElement("option");
+    option.innerHTML = `<option value="${x.name}">${x.name}</option>`;
+
+    citySelect.appendChild(option);
+  });
+
+  console.log(listaPara);
+
+  
+}
+*/
+
+ejemplo();
+
+async function mo() {
+  citySelect.addEventListener("click", (e) => {
+    console.log(citySelect.value);
+    //getWeather(citySelect.value);
+  });
 }
 
-getCats();
-*/
+mo();
