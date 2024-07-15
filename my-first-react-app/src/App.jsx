f874860480b0e3de25f1de31345046d81638ea04
Jose Crespo <rocj1209@gmail.com>
2024-07-15 18:00:17 -0400

    add test with vitest

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,14 +1,24 @@
-// import './App.css'
+// import "./App.css";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <p className="read-the-docs">Hello World!</p>
+//     </>
+//   );
+// }
 
-  return (
-    <>
-      <p className="read-the-docs">
-        Hello World!
-      </p>
-    </>
-  )
-}
+// export default App;
+import { render, screen } from "@testing-library/react";
 
-export default App
+import App from "./App";
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App title="React" />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
