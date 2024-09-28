ea043b24fdcb462277f3ee5b706f1dd7ea32cf2d
Jose Crespo <rocj1209@gmail.com>
2024-09-28 17:09:37 -0400

    add style, components im views

--- template-ejs/app.js
+++ template-ejs/app.js
@@ -1,13 +1,33 @@
 import express from "express";
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.disable("x-powered-by"); // deshabilitar el header X-Powered-By: Express
 
 app.set("view engine", "ejs");
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 app.get("/", (req, res) => {
-  res.render("app", { foo: "FOO" });
+  res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { links: links, users: users });
 });
 
 const PORT = process.env.PORT ?? 1234;
