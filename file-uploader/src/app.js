0eadf36987f2c6f2e87e1fcfd4486228cd1ca149
Jose Crespo <rocj1209@gmail.com>
2025-03-28 19:08:53 -0400

    in file-uploader: change compose.yml, README.md

---
+++ file-uploader/src/app.js
@@ -0,0 +1,29 @@
+const path = require('node:path');
+const express = require('express');
+
+// const session = require('express-session');
+// const passport = require('passport');
+
+const routes = require('./routes/index.js');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
+// app.use(passport.session());
+
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use(routes);
+
+const PORT = process.env.PORT_NODE_SERVER ?? 3000;
+const IP_NODE_SERVER = process.env.IP_NODE_SERVER ?? 'localhost';
+
+app.listen(PORT, () => {
+  console.log(`http://${IP_NODE_SERVER}:${PORT}`);
+});
---
+++ file-uploader/src/controllers/indexController.js
@@ -0,0 +1,21 @@
+// const db = require("../db/query.js");
+// const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('../db/generated/prisma_client');
+
+const prisma = new PrismaClient();
+
+async function getIndex (req, res) {
+  const manyUser = await prisma.user.findMany();
+  console.log(manyUser);
+  res.render('pages/index.ejs');
+  /*
+  if (req.isUnauthenticated()) {
+    res.render("pages/index.ejs", { messages });
+   } else {
+    // console.log(req)
+    res.render("pages/index.ejs", { user: req.user, messages });
+   }
+*/
+}
+
+module.exports = { getIndex };
