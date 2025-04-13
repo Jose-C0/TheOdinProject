204110a46d07a817dd31059cc3ac2dfb5ed4ce71
Jose Crespo <rocj1209@gmail.com>
2025-04-12 23:09:16 -0400

    b fup-react: only use one file

--- members-only/src/controllers/logOutController.js
+++
@@ -1,9 +0,0 @@
-async function getLogout(req, res, next) {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-}
-module.exports = { getLogout }
--- testing-practice/test/capitalize.test.js
+++
@@ -1,8 +0,0 @@
-const capitalize = require("../src/capitalize.js");
-
-// 1. capitalize: A function that takes a string and
-//    returns it with the first character capitalized.
-test("1. First character is capitalized", () => {
-  const value = capitalize("testString");
-  expect(capitalize("TestString")[0]).toMatch(/[A-Z]/);
-});
